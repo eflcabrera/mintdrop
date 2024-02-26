73137fa7babd6c563b74864034d8a0cca45709cd
Ezequiel Cabrera <eze.fcabrera@gmail.com>
2024-02-26 19:09:52 -0300

    top_bar Agrega scaffolds y topbars a cada página

--- app/src/main/java/com/eflc/mintdrop/ui/income/Income.kt
+++ app/src/main/java/com/eflc/mintdrop/ui/income/Income.kt
@@ -1,5 +1,6 @@
 package com.eflc.mintdrop.ui.income
 
+import android.annotation.SuppressLint
 import android.net.Uri
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -12,16 +13,17 @@ import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.eflc.mintdrop.models.ExpenseCategory
@@ -30,51 +32,52 @@ import com.eflc.mintdrop.ui.card.CategoryCard
 import com.eflc.mintdrop.utils.Constants
 import com.google.gson.Gson
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun IncomeScreen(navComposable: NavController) {
     val homeViewModel: IncomeViewModel = hiltViewModel()
     val categories by homeViewModel.state.collectAsState()
     val sheet = Constants.INCOME_SHEET_NAME
 
-    Box(
-        contentAlignment = Alignment.Center,
-        modifier = Modifier
-            .width(400.dp)
-            .height(36.dp)
-    ) {
-        Text(
-            text = Constants.INCOME_SHEET_NAME,
-            fontSize = 20.sp,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier
-                .align(alignment = Alignment.Center)
-        )
-        if (categories.isEmpty()) {
-            CircularProgressIndicator(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .wrapContentSize(align = Alignment.Center)
-                    .padding(top = 100.dp)
-            )
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            CenterAlignedTopAppBar(title = { Text(text = sheet) })
         }
-    }
-
-    LazyVerticalGrid(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(top = 50.dp),
-        columns = GridCells.Adaptive(140.dp),
-        verticalArrangement = Arrangement.Center,
-        contentPadding = PaddingValues(horizontal = 20.dp, vertical = 80.dp)
     ) {
-        items(categories) { category: ExpenseCategory ->
-            ExpenseCategoryCard(
-                category = category,
-                onClick = {
-                    val categoryJson = Uri.encode(Gson().toJson(it))
-                    navComposable.navigate(route = AppScreens.CategoryScreen.route + "/$categoryJson/$sheet")
-                }
-            )
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .width(400.dp)
+                .height(36.dp)
+        ) {
+            if (categories.isEmpty()) {
+                CircularProgressIndicator(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .wrapContentSize(align = Alignment.Center)
+                        .padding(top = 100.dp)
+                )
+            }
+        }
+
+        LazyVerticalGrid(
+            modifier = Modifier
+                .fillMaxSize(),
+            columns = GridCells.Adaptive(140.dp),
+            verticalArrangement = Arrangement.Center,
+            contentPadding = PaddingValues(start = 20.dp, end = 20.dp, top = 120.dp, bottom = 140.dp)
+        ) {
+            items(categories) { category: ExpenseCategory ->
+                ExpenseCategoryCard(
+                    category = category,
+                    onClick = {
+                        val categoryJson = Uri.encode(Gson().toJson(it))
+                        navComposable.navigate(route = AppScreens.CategoryScreen.route + "/$categoryJson/$sheet")
+                    }
+                )
+            }
         }
     }
 }
