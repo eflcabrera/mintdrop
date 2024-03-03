aefe052d4645a626b07da83346e1e1ead92a53b3
Ezequiel Cabrera <38702934+eflcabrera@users.noreply.github.com>
2024-03-03 16:14:23 -0300

    Agregar top bar a la aplicación (#1)
    * top_bar Componetiza NavigationBar
    
    * top_bar Agrega scaffolds y topbars a cada página

--- app/src/main/java/com/eflc/mintdrop/ui/category/Category.kt
+++ app/src/main/java/com/eflc/mintdrop/ui/category/Category.kt
@@ -1,23 +1,22 @@
 package com.eflc.mintdrop.ui.category
 
+import android.annotation.SuppressLint
 import android.net.Uri
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -27,6 +26,8 @@ import com.eflc.mintdrop.navigation.AppScreens
 import com.eflc.mintdrop.ui.card.CategoryCard
 import com.google.gson.Gson
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CategoryScreen(
     navComposable: NavController,
@@ -35,44 +36,36 @@ fun CategoryScreen(
 ) {
     val subCategories: List<ExpenseSubCategory> = category.subCategories.subList(1, category.subCategories.lastIndex)
 
-    Box(
-        modifier = Modifier
-            .width(400.dp)
-            .height(36.dp)
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            CenterAlignedTopAppBar(title = { Text(text = category.name) })
+        }
     ) {
-        Text(
-            text = category.name,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier
-                .padding(start = 8.dp)
-                .align(alignment = Alignment.Center)
-        )
-    }
-
-    if (subCategories.isEmpty()) {
-        CircularProgressIndicator(
-            modifier = Modifier
-                .fillMaxSize()
-                .wrapContentSize(align = Alignment.Center)
-        )
-    }
-
-    LazyVerticalGrid(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(top = 36.dp),
-        columns = GridCells.Fixed(2),
-        verticalArrangement = Arrangement.Center,
-        contentPadding = PaddingValues(horizontal = 20.dp)
-    ) {
-        items(subCategories) { subcategory: ExpenseSubCategory ->
-            ExpenseSubCategoryCard(
-                subCategory = subcategory,
-                onClick = {
-                    val subcategoryJson = Uri.encode(Gson().toJson(it))
-                    navComposable.navigate(route = AppScreens.ExpenseEntryScreen.route + "/$subcategoryJson/$sheet")
-                }
+        if (subCategories.isEmpty()) {
+            CircularProgressIndicator(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .wrapContentSize(align = Alignment.Center)
             )
+        }
+
+        LazyVerticalGrid(
+            modifier = Modifier
+                .fillMaxSize(),
+            columns = GridCells.Fixed(2),
+            verticalArrangement = Arrangement.Center,
+            contentPadding = PaddingValues(start = 20.dp, end = 20.dp, top = 120.dp, bottom = 140.dp)
+        ) {
+            items(subCategories) { subcategory: ExpenseSubCategory ->
+                ExpenseSubCategoryCard(
+                    subCategory = subcategory,
+                    onClick = {
+                        val subcategoryJson = Uri.encode(Gson().toJson(it))
+                        navComposable.navigate(route = AppScreens.ExpenseEntryScreen.route + "/$subcategoryJson/$sheet")
+                    }
+                )
+            }
         }
     }
 }
